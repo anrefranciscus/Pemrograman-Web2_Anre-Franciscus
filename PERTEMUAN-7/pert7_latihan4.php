56b411a1f9f5c5f18a9e089a60667775047c6825
anrefranciscus <58438205+anrefranciscus@users.noreply.github.com>
2021-03-16 01:21:45 +0700

    Create pert7_latihan4.php

---
+++ PERTEMUAN-7/pert7_latihan4.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Penggunaan Is Array</title>
+</head>
+<body>
+	<?php 
+	$var = array(1,2,3,4,5,6,7);
+	$scan = is_array($var);
+	if ($scan === false) {
+		$status = "Bukan";
+	}else{
+		$status = "";
+	}
+	echo "\$var = array(1,2,3,4,5,6,7)";
+	echo "<br>";
+	echo "Variable \$var $status merupakan array";
+
+
+
+	 ?>
+</body>
+</html>
